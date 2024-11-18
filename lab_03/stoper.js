7ff3ca34c7d9fdf2ca3d230daf3f03dc3021c9fd
odyszewska <151792408+odyszewska@users.noreply.github.com>
2024-11-18 12:37:20 +0100

    fixed stoper

--- lab_03/stoper.js
+++ lab_03/stoper.js
@@ -1,41 +1,49 @@
-const timer = document.getElementById('time');
-const start = document.querySelector("#start")
-const stop = document.querySelector("#stop")
-const reset = document.querySelector("#reset")
-let time = 0;
-let paused = true;
+const time = document.querySelector("#time");
+const start = document.querySelector("#start");
+const stop = document.querySelector("#stop");
+const reset = document.querySelector("#reset");
 
+let startTime = 0;
+let elapsedTime = 0;
+let stoped = true;
+let min = 0;
+let sec = 0;
 
-start.addEventListener("click",() => {
-    if(paused){
-        paused = false;
+start.addEventListener("click", () => {
+    if(stoped){
+        stoped = false;
+        startTime = Date.now() - elapsedTime;
         intervalId = setInterval(updateTime, 1000);
     }
 });
-stop.addEventListener("click",() => {
-    if(!paused){
-        paused = true;
-        clearInterval(intervalId)
+stop.addEventListener("click", () => {
+    if(!stoped){
+        stoped = true;
+        elapsedTime = Date.now() - startTime;
+        clearInterval(intervalId);
     }
 });
-reset.addEventListener("click",() => {
-    paused = true;
-    clearInterval(intervalId)
-    time = 0;
-    seconds = 0;
-    minutes = 0;
-    timer.innerHTML = `0s`;
+reset.addEventListener("click", () => {
+    stoped = true;
+    clearInterval(intervalId);
+    startTime = 0;
+    elapsedTime = 0;
+    min = 0;
+    sec = 0;
+    time.textContent = "0s";
 });
-
 
 function updateTime(){
-    const minutes = Math.floor(time/60);
-    let seconds = time % 60
-    if (minutes>0){
-        timer.innerHTML = `${minutes}m ${seconds}s`;
+    elapsedTime = Date.now() - startTime;
+
+    sec = Math.floor((elapsedTime / 1000) % 60);
+    min = Math.floor((elapsedTime / (1000 * 60)) % 60);
+
+    if (min >= 1){
+        time.textContent = `${min}m ${sec}s`;
     }
     else{
-        timer.innerHTML = `${seconds}s`;
+        time.textContent = `${sec}s`;
     }
-    time++;
+    
 }
